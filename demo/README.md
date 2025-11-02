d1e3d363aacf672f9ff6005553713662ae0ac35a
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-11-02 21:22:08 +0000

    Updated routes

--- demo/README.md
+++ demo/README.md
@@ -1,7 +1,3 @@
-# Settings
-- Press 'F9', click 'Load XML Decals', and select 'images/1-2250.xml' (1:2250 image scale)
-- In Netedit, press 'F10' and ensure 'lefthand' is true to simulate UK road system
-
 # Network Guide
 Lanes names:
 - (Street Name)\_(Lane Number)\_(Segment Number)
--- demo/scripts/generate_routes.py
+++ demo/scripts/generate_routes.py
@@ -1,7 +1,93 @@
 import os
+import subprocess
+import shutil
+from pathlib import Path
 
-NETWORK = 'network.net.xml'
-DURATION = 3600
+NET_NAME = 'demo'
+DIR_PREFIX = '../'
+NETWORK = f'{DIR_PREFIX}networks/{NET_NAME}.net.xml'
+DURATION = '1000' # Seconds - for how long new entities should be spawned
+SEED = '42'
+
+# Find default SUMO tools
 SUMO_HOME = os.environ.get('SUMO_HOME')
 TOOLS = os.path.join(SUMO_HOME, 'tools')
 
+if not SUMO_HOME or not os.path.exists(TOOLS):
+    raise EnvironmentError('SUMO_HOME is not set, or SUMO tools not found.')
+
+# Create directory in routes folder
+Path(f'{DIR_PREFIX}routes/{NET_NAME}/').mkdir(exist_ok=True)
+
+
+def run_command(cmd: list[str]) -> None:
+    '''Run command and stream its output'''
+    print(f'Running > {" ".join(cmd)}')
+    res = subprocess.run(cmd, text=True)
+    if res.returncode != 0:
+        raise RuntimeError(f'Command failed: {" ".join(cmd)}')
+
+
+
+
+# POISSSON
+
+# RANDOM DEPART
+
+# ADD EXTRA PEDESTRIAN WALK PATH GOING INTO/FROM SCHOOL BUILDING AND PARKS
+
+
+
+
+
+vehicles = [
+    {
+        'name': 'car',
+        'args': [
+            '-n', NETWORK,
+            '-e', DURATION,
+            '-p', '5',
+            '-o', f'{DIR_PREFIX}routes/{NET_NAME}/car.trips.xml',
+            '-r', f'{DIR_PREFIX}routes/{NET_NAME}/car.rou.xml',
+            '--fringe-factor', 'max', # ensure the vehicles only spawn from the very edges, rather than the middle of the road
+            '--prefix', 'car',
+            '--seed', SEED
+        ]
+    },
+    {
+        'name': 'bicycle',
+        'args': [
+            '-n', NETWORK,
+            '-e', DURATION,
+            '-p', '10',
+            '-o', f'{DIR_PREFIX}routes/{NET_NAME}/bicycle.trips.xml',
+            '-r', f'{DIR_PREFIX}routes/{NET_NAME}/bicycle.rou.xml',
+            '--fringe-factor', 'max',
+            '--prefix', 'bicycle',
+            '--vehicle-class', 'bicycle',
+            '--seed', SEED
+        ]
+    },
+    {
+        'name': 'pedestrian',
+        'args': [
+            '-n', NETWORK,
+            '-e', DURATION,
+            '-p', '3',
+            '-o', f'{DIR_PREFIX}routes/{NET_NAME}/pedestrian.trips.xml',
+            '-r', f'{DIR_PREFIX}routes/{NET_NAME}/pedestrian.rou.xml',
+            '--fringe-factor', 'max',
+            '--prefix', 'pedestrian',
+            '--pedestrians',
+            '--seed', SEED
+        ]
+    }
+]
+
+for v in vehicles:
+    print(f'Generating {v["name"]} trips')
+    random_trip = os.path.join(TOOLS, 'randomTrips.py')
+    duarouter = shutil.which('duarouter')
+
+    # generate trip (Start A ==> Finish B) and route (Start A -> 1 -> 2 -> ... -> n -> Finish B)
+    run_command(['python', random_trip] + v['args'])
