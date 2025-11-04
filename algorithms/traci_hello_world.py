71d18a8e6784611e23d8e43dfed0b068f80cfb6f
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-11-04 21:28:57 +0000

    Minor change

--- algorithms/traci_hello_world.py
+++ algorithms/traci_hello_world.py
@@ -6,16 +6,22 @@ if not os.environ.get('SUMO_HOME'):
 sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
 import traci
 
+SIMULATION_TIME = 3600
+STEP_LENGTH = 0.10
+TOTAL_STEPS = int(SIMULATION_TIME / STEP_LENGTH)
+
 sumo_config = [
     'sumo-gui',
     '-c', '../simulation/simulation.sumocfg',
+    '--step-length', str(STEP_LENGTH),
+    '--delay', '100',
+    '--lateral-resolution', '0'
 ]
 
 traci.start(sumo_config)
 
-TOTAL_STEPS = 10000
-
 for step in range(TOTAL_STEPS):
     traci.simulationStep()
+    print(f'Step: {step}')
 
 traci.close()
