9d9ab17f27181073ee12e1824d89cd667f6e60e5
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-11-09 17:02:39 +0000

    Update get_peds function

--- algorithms/fixed_timer.py
+++ algorithms/fixed_timer.py
@@ -28,12 +28,23 @@ detector_ids = [
     'CJ_1_EB_1',
     'CJ_1_WB_1',
 ]
+# IDs of crossings and the corresponding walking areas
+crossing_ids = [
+    (':CJ_1_c0', ':CJ_1_w0', ':CJ_1_w1'),
+    (':CJ_1_c1', ':CJ_1_w1', ':CJ_1_w2'),
+    (':CJ_1_c2', ':CJ_1_w2', ':CJ_1_w3'),
+    (':CJ_1_c3', ':CJ_1_w3', ':CJ_1_w0'),
+]
 
 
-# Get number of pedestrians - useful link that could help https://sumo.dlr.de/docs/Tutorials/TraCIPedCrossing.html
-def get_peds():
-    ...
-
+def get_peds(crossing_id: str, waiting_edge_ids: tuple[str, str]) -> int:
+    # get number of pedestrians waiting to use a crossing 
+    peds = traci.edge.getLastStepPersonIDs(waiting_edge_ids[0]) + traci.edge.getLastStepPersonIDs(waiting_edge_ids[1])
+    count = 0
+    for ped in peds:
+        if traci.person.getNextEdge(ped) == crossing_id:
+            count += 1
+    return count
 
 def get_current_tls_phase(tls_id: str) -> str:
     return traci.trafficlight.getPhase(tls_id)
@@ -43,23 +54,30 @@ def get_detector_queue_length(detector_id: str) -> int:
     return traci.lanearea.getLastStepVehicleNumber(detector_id)
 
 
-def get_state(tls: str, *args: str) -> tuple[str, ...]:
+def get_state(tls: str, detectors: list[str], crossings: list[tuple[str]]) -> tuple[str, ...]:
     # get current phase of traffic light system
     tls_phase = get_current_tls_phase(tls)
 
+    # get number of vehicles waiting in each queue
     queue_lengths = []
-    for detector in args:
+    for detector in detectors:
         size = get_detector_queue_length(detector)
         queue_lengths.append(size)
 
-    return (tls_phase, ) + tuple(queue_lengths)
+    # get number of pedestrians waiting to use each crossing
+    num_peds = []
+    for crossing in crossings:
+        count = get_peds(crossing[0], crossing[1:])
+        num_peds.append(count)
+
+    return (tls_phase, ) + tuple(queue_lengths) + tuple(num_peds)
 
 
 traci.start(sumo_config)
 
 for step in range(TOTAL_STEPS):
     traci.simulationStep()
-    state = get_state(tls_id, *detector_ids)
+    state = get_state(tls_id, detector_ids, crossing_ids)
     print(f'Step: {step}, State: {state}')
 
 traci.close()
