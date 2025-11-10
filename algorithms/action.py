c6aca550a291b9a4464c1e9652138a91950d6840
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-11-10 22:10:21 +0000

    Redefined action space, and began Q learning section

--- algorithms/action.py
+++ algorithms/action.py
@@ -7,10 +7,45 @@ sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
 import traci
 
 
-# TODO: Implement an action space which is just 'keep' or 'next' phase
+# 8 traffic light phases defined in 'main.net.xml'
+tls_logic = """
+<tlLogic id="CJ_1" type="static" programID="0" offset="0">
+    <phase duration="30" state="gGggGgrrrrgGggGgrrrrrrrr"/>
+    <phase duration="3"  state="yyyyyyrrrryyyyyyrrrrrrrr"/>
+    <phase duration="2"  state="rrrrrrrrrrrrrrrrrrrrrrrr"/>
+    <phase duration="30" state="rrrrrrgGggrrrrrrgGggrrrr"/>
+    <phase duration="3"  state="rrrrrryyyyrrrrrryyyyrrrr"/>
+    <phase duration="2"  state="rrrrrrrrrrrrrrrrrrrrrrrr"/>
+    <phase duration="10" state="rrrrrrrrrrrrrrrrrrrrGGGG"/>
+    <phase duration="10" state="rrrrrrrrrrrrrrrrrrrrrrrr"/>
+</tlLogic>
+"""
+
+# only phase index 0, 3, and 6 are available actions
+#
+# graceful switch defines the phases that should be run when switching away from that action
+# list[(phase index, duration), ...]
+ACTION_SPACE = {
+    # north bound and south bound
+    0: {
+        'min_duration': 10,
+        'graceful_switch': [(1,3),(2,2)]
+    },
+    # east bound and west bound
+    3: {
+        'min_duration': 10,
+        'graceful_switch': [(4,3),(5,2)]
+    },
+    # pedestrians
+    6: {
+        'min_duration': 10,
+        'graceful_switch': [(7,10)]
+    }
+}
 
 
-def perform_action(tls_id: str, current_phase: int, action: int) -> None:
-    # 8 traffic light phases defined in 'main.net.xml'
-    # index 2, 5, and 7 are identical (all red lights)
+def perform_action(tls_id: str, prev_action: int | None, action: int) -> int:
+    # peform action changing phase on the traffic light system, and return total duration
     traci.trafficlight.setPhase(tls_id, action)
+    duration = 
+    traci.trafficlight.setPhaseDuration(tls_id, duration)
--- algorithms/q_learning.py
+++ algorithms/q_learning.py
@@ -6,25 +6,72 @@ if not os.environ.get('SUMO_HOME'):
 sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
 import traci
 
+import numpy as np
+import random
 from settings import SUMO_CONFIG, TOTAL_STEPS, tls_id, detector_ids, crossing_ids
-from state import get_state, get_current_tls_phase, get_all_waiting_vehicles, get_all_waiting_peds
-from action import perform_action
+from state import get_state, get_all_waiting_vehicles, get_all_waiting_peds
+from action import ACTION_SPACE, perform_action
 from reward import get_reward
 
-... FIX POISSION RANDOM TRIP GENERATION!
+
+# TODO
+# REMINDER
+# FIX POISSION RANDOM TRIP GENERATION!!!
+#
 
 
-traci.start(SUMO_CONFIG)
+Q = {} # {(state, action): value}
+
+# Q-learning hyperparameters
+ALPHA = 0.1 # learning rate
+GAMMA = 0.9 # discount factor
+EPSILON = 0.2 # exploration rate
+EPISODES = 100
+
+
+def get_q(state: tuple[int, ...], action: int) -> float:
+    # return Q-value of state and action combination
+    return Q.get((state, action), 0.0)
+
+
+def choose_action(state: tuple[int, ...]) -> int:
+    # choose action using an epsilon-greedy policy
+    if random.random() < EPSILON:
+        # exploration - choose random action
+        return random.choice(ACTION_SPACE)
+    else:
+        # exploitation - choose action with highest Q-value
+        qs = [get_q(state, a) for a in ACTION_SPACE]
+        return ACTION_SPACE[np.argmax(qs)]
+
+
+def update_q(state: tuple[int, ...], action: int, reward: float, next_state: tuple[int, ...]) -> None:
+    # update Q-value of state and action combinatoin based on reward and next state
+    old_q = get_q(state, action)
+    best_next = max([get_q(next_state, a) for a in ACTION_SPACE])
+    Q[(state, action)] = old_q + ALPHA * (reward + GAMMA * best_next - old_q)
+
 
 total_reward = 0
 
+traci.start(SUMO_CONFIG)
+
 for step in range(TOTAL_STEPS):
+    state = get_state(tls_id, detector_ids, crossing_ids)
+    action = choose_action(state)
+    
+    ....duration = perform_action(tls_id, action)
+
     traci.simulationStep()
     
-    state = get_state(tls_id, detector_ids, crossing_ids)
+    next_state = get_state(tls_id, detector_ids, crossing_ids)
     reward = get_reward(get_all_waiting_vehicles(detector_ids), get_all_waiting_peds(crossing_ids))
+    update_q(state, action, reward, next_state)
+
     total_reward += reward
 
     print(f'Step: {step}, State: {state}, Reward: {reward}')
+
+print(f'Total Reward: {total_reward}')
 
 traci.close()
