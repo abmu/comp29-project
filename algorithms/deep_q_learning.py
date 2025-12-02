9378aa1aa79e3089da98faaf7836441ae8a222a0
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-02 21:44:19 +0000

    Got started on replay buffer class

--- algorithms/deep_q_learning.py
+++ algorithms/deep_q_learning.py
@@ -1,7 +1,12 @@
 import torch
 import torch.nn as nn
+import random
+from collections import deque
 
 
+"""
+    Deep Q-Network model
+"""
 class DQN(nn.Module):
     def __init__(self, state_dim: int, action_dim: int):
         super().__init__()
@@ -17,3 +22,16 @@ class DQN(nn.Module):
         return self.model(x)
     
 
+"""
+    Replay buffer memory
+"""
+class ReplayBuffer:
+    def __init__(self, capacity: int = 1024):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state: tuple[int, ...], action: int, reward: float, next_state: tuple[int, ...], duration: int, done: bool):
+        self.buffer.append((state, action, reward, next_state, duration, done))
+
+    def sample(self, batch_size: int):
+        batch = random.sample(self.buffer, batch_size)
+        
