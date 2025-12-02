0996a530912bd63928523c5eeec2ae24427b5ff8
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-02 21:33:32 +0000

    Created very simple neural network

--- algorithms/deep_q_learning.py
+++ algorithms/deep_q_learning.py
@@ -1,1 +1,19 @@
 import torch
+import torch.nn as nn
+
+
+class DQN(nn.Module):
+    def __init__(self, state_dim: int, action_dim: int):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_dim)
+        )
+
+    def forward(self, x: tuple[int, ...]):
+        return self.model(x)
+    
+
