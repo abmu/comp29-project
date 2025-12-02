34fc97fddf876a94cf3a7db79dbc3e01f2488b76
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-02 21:24:25 +0000

    Removed old files and added bug route

--- simulation/scripts/generate_routes.py
+++ simulation/scripts/generate_routes.py
@@ -8,7 +8,7 @@ from utils import run_command
 
 
 # TODO
-# - BUG: fix bug in network where bikes at CJ_1 can sometimes cause congestion
+# - BUG: fix bug in network where bikes at CJ_1 can sometimes cause congestion, e.g. route 388
 # - Improve pedestrians accuracy by varying fringe-factor -- simulates pedestrians going into/out of buildings
 # - Manually adjust weight of edges to make certain edges more likely to spawn, rather than random choice -- simulates commonly used/ main roads
 
