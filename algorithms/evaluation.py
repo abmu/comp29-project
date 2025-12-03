b03df13edcee55c4a4ad3995a96f6225dd7e8136
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-03 15:17:49 +0000

    TODO

--- algorithms/evaluation.py
+++ algorithms/evaluation.py
@@ -4,7 +4,7 @@
 # - Extend network, and think about network communication with the traffic light systems to improve performance
 #
 # Implement evaluation framework
-# - Run each algorithm in parallel on the same dataset
+# - Run each algorithm in parallel on the same dataset -- Fixed number of episodes, start a new traci siumlation, change route once every algorithm finishes that episode
 # - Evaluate performance in new seeds, with different traffic densities (more/less pedestrians, more/less vehicles)
 # - Comment on different weights to prioritise either pedestrians or vehicles (currently equal)
 # - Explore other methods such as other Q-learning techniques (Double DQN, Dueling DQN) and actor-critic
