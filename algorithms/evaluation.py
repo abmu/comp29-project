13b3bcdfa6c9cbb72d28819738195f889b386cc7
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-03 15:09:57 +0000

    Added TODO

--- algorithms/evaluation.py
+++ algorithms/evaluation.py
@@ -1,5 +1,10 @@
 # TODO
+# - Complete DQN
+# - Examine using traffic lights vs zebra crossings
+# - Extend network, and think about network communication with the traffic light systems to improve performance
+#
 # Implement evaluation framework
 # - Run each algorithm in parallel on the same dataset
 # - Evaluate performance in new seeds, with different traffic densities (more/less pedestrians, more/less vehicles)
 # - Comment on different weights to prioritise either pedestrians or vehicles (currently equal)
+# - Explore other methods such as other Q-learning techniques (Double DQN, Dueling DQN) and actor-critic
