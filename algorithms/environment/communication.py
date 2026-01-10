72326ee49aacb903dbd4c751a3fa48b7af74c648
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2026-01-10 16:00:07 +0000

    Basic state bus setup

---
+++ algorithms/environment/communication.py
@@ -0,0 +1,18 @@
+from .settings import TLS_IDS
+
+
+class StateBus:
+    def __init__(self) -> None:
+        self.states = {}  # {agent_id: state}
+
+
+    def publish(self, agent_id: str, state: tuple[int, ...]) -> None:
+        # publish state to bus
+        self.states[agent_id] = state
+
+    
+    def read(self, agent_id: str) -> dict[str, tuple[int, ...]]:
+        # return the states of the adjacent agents
+        return {
+            a: self.states[a] for a in TLS_IDS[agent_id]['adjacent']
+        }
